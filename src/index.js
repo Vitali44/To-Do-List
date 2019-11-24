0e1ad6d80dcf36841ca1dcb50c74ac2d24793a73
Vitali Barycheuski <vitaliyborichev@gmail.com>
2019-11-24 20:00:19 +0300

    Second comment

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import TodoList from "./ToDoList";
+import TodoList from "./ToDoList"; //добавили файл с созданным элементом
 
 var destination = document.querySelector("#container");
 
 ReactDOM.render( //рендеринг реакт элемента. Обновление элемента
     <div>
-        <TodoList/>
-    </div>,
+        <TodoList/> 
+    </div>, //использование компонента из импорта
     destination
 
 );
