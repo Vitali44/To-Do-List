6e4875bab2c8649a1dded04cda6e4ab1eb567651
Vitali Barycheuski <vitaliyborichev@gmail.com>
2019-11-25 15:08:16 +0300

    Delete and added are work

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 
 class TodoItems extends Component {
-    createTasks(item) {
-        return <li key={item.key}>{item.text}</li>
+    constructor(props) {
+        super(props);
+
+        this.createTasks = this.createTasks.bind(this);
     }
 
+
+    createTasks(item) {
+        return <li onClick={() => this.delete(item.key)} 
+        key={item.key}>{item.text}</li>
+    }
+
+    delete(key) {
+        this.props.delete(key);
+    }
+
+
     render() {
-        var todoEntries = this.props.entries;
-        var listItems = todoEntries.map(this.createTasks);
+        let todoEntries = this.props.entries;
+        let listItems = todoEntries.map(this.createTasks);
 
         return (
             <ul className="theList">
